7e95ff672f03e46d477b9a2c0a882f49299bdc87
Phong Tran <phong.tran@edge-works.net>
2019-03-01 10:08:31 +0700

    Procfile

--- todolist/settings.py
+++ todolist/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -168,4 +169,7 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211'
     }
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
