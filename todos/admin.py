f1fce97ed76be5e50fbda2cb358e26dc535518b1
Phong Tran <phong.tran@edge-works.net>
2019-03-04 14:14:15 +0700

    v2.5.6 minor fix#4

--- todos/admin.py
+++ todos/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from .models import Todo, TaskImage
 # from rest_framework.authtoken.admin import TokenAdmin
 
-class TodoAdmin(admin.ModelAdmin):
-    pass
-    
 class TaskAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Todo, TodoAdmin)
+class TodoAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(TaskImage, TaskAdmin)
+
+admin.site.register(Todo, TodoAdmin)
 
 # TokenAdmin.raw_id_fields = ('user',)
